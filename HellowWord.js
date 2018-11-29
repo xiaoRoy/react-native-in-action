709a31b36d069d9c9f53fd5267f7a075cacf88e8
Jerry Li <xiaoroyg@163.com>
2018-11-29 23:52:03 +0800

    Finished Hello World.

---
+++ HellowWord.js
@@ -0,0 +1,12 @@
+import React, { Componet } from 'react';
+import {Text, View} from 'react-native'
+
+export default class HelloWord extends Componet {
+    render() {
+        return {
+            <View>
+                <Text>Hello Word !</Text>
+            </View>
+        }
+    }
+}
