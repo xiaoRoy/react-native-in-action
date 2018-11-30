970c375e0a7228334b160942720a0276ab95b631
Jerry Li <xiaoroyg@163.com>
2018-11-30 23:54:13 +0800

    Init WeatherProject

---
+++ WeatherProject.js
@@ -0,0 +1,16 @@
+import React, { Componet } from "react";
+import {StyleSheet, Text, View, TextInput} from "react-native"
+
+class WeatherProject extends Componet {
+    constructor(props){
+        super(props)
+        this.state = {zip: ""}
+    }
+
+   
+}
+ const styles = StyleSheet.create({
+     container : {
+         
+     }
+ })
